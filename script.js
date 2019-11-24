57662a2b8daada8c6f2f4b80811ab41653b3f837
Nico Tanne <el0quenz92@gmail.com>
2019-11-25 00:10:07 +0100

    Added some Comments into the Javascript File

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 let todoList = {
-  todos: ["item1", "item2", "item3"],
-  displayTodos: function() {
-    console.log("My Todos", this.todos)
-  },
-  
-  addTodo: function(todo) {
-    this.todos.push(todo);
-    this.displayTodos();
-  },
-  
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
-    this.displayTodos();
-  },
-  
-  deleteTodo: function(position) {
-    this.todos.splice(position, 1);
-    this.displayTodos();
-    
-  }
+	todos: [ 'item1', 'item2', 'item3' ], //Creating an Array
+	displayTodos: function() {
+		//Adding a function to show the Display of the Array
+		console.log('My Todos', this.todos);
+	},
 
+	addTodo: function(todo) {
+		//Function to add a new item to the Array
+		this.todos.push(todo); //"this" to call the todos from the function itself
+		this.displayTodos();
+	},
+
+	changeTodo: function(position, newValue) {
+		//Function to change an item in the Array List
+		this.todos[position] = newValue;
+		this.displayTodos();
+	},
+
+	deleteTodo: function(position) {
+		//Function to delete an item in the Array List
+		this.todos.splice(position, 1);
+		this.displayTodos();
+	}
 };
